7fd8bc48573625588a78817f7b8e4e01c9ea1658
monellz <rxzhong@outlook.com>
2026-01-07 15:36:42 +0800

    fix bug in root bootstrap

--- bootstrap/arch/bootstrap.sh
+++ bootstrap/arch/bootstrap.sh
@@ -15,10 +15,11 @@ SCRIPTS_DIR=$PROJECT_DIR/scripts
 
 SNAPSHOTS_DIR=/.snapshots
 
-ts=$(date +$Y$m$d-$H$M$S)
+ts=$(date +%Y%m%d-%H%M%S)
 
 # snapshot first
-pacman -S rsync
+info "Snapshot all"
+pacman -S --noconfirm rsync
 # boot
 rsync -a --mkpath --delete /boot/ "/.bootbackup/init_${ts}_boot"
 btrfs subvolume snapshot -r / $SNAPSHOTS_DIR/init_${ts}_root
@@ -30,35 +31,45 @@ btrfs subvolume snapshot -r /home $SNAPSHOTS_DIR/init_${ts}-root
 # copy $SHELL_DIR/etc/pacman.d/mirrorlist  /etc/pacman.d/mirrorlist
 
 
-# necessary packages
-pacman -S vim iwd git openssh
-pacman -S grub efibootmgr
-pacman -S os-prober
 
+# locale
+info "Adjust locale"
+copy $SHELL_DIR/etc/locale.gen /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "KEYMAP=us" > /etc/vconsole.conf
+
+# time
+info "Adjust time"
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+timedatectl set-ntp true
+hwclock --systohc --utc
+
+
+# necessary packages
+info "Install necessary packages"
+pacman -S --noconfirm vim iwd git openssh
+pacman -S --noconfirm grub efibootmgr
+pacman -S --noconfirm os-prober
 
 if grep -q "GenuineIntel" /proc/cpuinfo; then
     info "Install intel ucode"
-    pacman -S intel-ucode
+    pacman -S --noconfirm intel-ucode
 elif grep -q "AuthenticAMD" /proc/cpuinfo; then
     info "Install amd ucode"
-    pacman -S amd-ucode
+    pacman -S --noconfirm amd-ucode
 else
     error "Unsupported CPU"
     exit 1
 fi
 
-# time
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-timedatectl set-ntp true
-hwclock --systohc --utc
-
-# locale
-localectl set-locale LANG=en.asdasd
-localectl set-keymap us
-
+info "Grub install"
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ARCH
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# user 
-# useradd -m -G wheel,audio,video,input,storage -s /bin/bash zrx
+info "Create user"
+useradd -m -G wheel,audio,video,input,storage -s /bin/bash zrx || true
+passwd zrx
+
+ok "Root bootstrap finished"
 
