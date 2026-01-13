c917e44c945ebdc1c537ef1e998d1c11c907546d
monellz <rxzhong@outlook.com>
2026-01-13 14:15:54 +0800

    update encrypted files

--- scripts/set_local_git_config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-
-git config filter.crypt.clean  'openssl enc -aes-256-cbc -salt -pbkdf2 -pass env:PASS'
-git config filter.crypt.smudge 'openssl enc -d -aes-256-cbc -salt -pbkdf2 -pass env:PASS'
-git config filter.crypt.required true
