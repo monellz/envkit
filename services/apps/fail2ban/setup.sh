07c404b9ef63956da7b90fa549469e8ea2d9de4b
monellz <rxzhong@outlook.com>
2026-01-20 16:12:30 +0800

    add gitea

--- services/apps/fail2ban/setup.sh
+++ services/apps/fail2ban/setup.sh
@@ -14,18 +14,13 @@ sudo apt install -y fail2ban python3-systemd
 
 USE_SUDO=1 link ${SHELL_DIR}/jail.local /etc/fail2ban/jail.local
 
-USE_SUDO=1 link ${SHELL_DIR}/filter.d/caddy-webdav.conf /etc/fail2ban/filter.d/caddy-webdav.conf
-USE_SUDO=1 link ${SHELL_DIR}/filter.d/caddy-qbit.conf /etc/fail2ban/filter.d/caddy-qbit.conf
-USE_SUDO=1 link ${SHELL_DIR}/filter.d/jellyfin.conf /etc/fail2ban/filter.d/jellyfin.conf
-USE_SUDO=1 link ${SHELL_DIR}/filter.d/vaultwarden.conf /etc/fail2ban/filter.d/vaultwarden.conf
+for f in ${SHELL_DIR}/filter.d/*; do
+    USE_SUDO=1 link $f /etc/fail2ban/filter.d/$(basename $f)
+done
 
-USE_SUDO=1 link ${SHELL_DIR}/jail.d/caddy-webdav.local /etc/fail2ban/jail.d/caddy-webdav.local
-USE_SUDO=1 link ${SHELL_DIR}/jail.d/caddy-qbit.local /etc/fail2ban/jail.d/caddy-qbit.local
-USE_SUDO=1 link ${SHELL_DIR}/jail.d/jellyfin.local /etc/fail2ban/jail.d/jellyfin.local
-USE_SUDO=1 link ${SHELL_DIR}/jail.d/vaultwarden.local /etc/fail2ban/jail.d/vaultwarden.local
-
-
-
+for f in ${SHELL_DIR}/jail.d/*; do
+    USE_SUDO=1 link $f /etc/fail2ban/jail.d/$(basename $f)
+done
 
 # sudo systemctl restart fail2ban
 sudo fail2ban-client reload
